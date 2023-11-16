a29936c225106b47cf60abb1a4e6fbfa8275132f
Lucas Andreatta <drea.pierra@gmail.com>
2023-11-16 09:27:11 -0300

    pain, part 2

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,25 @@ int main(int argc, char* argv[]) {
     cxxopts::Options options("nbspcli", "A CLI for nitgen biometric devices");
     NBioAPI_HANDLE nbioApiHandle;
     NBioAPI_RETURN nbioApiReturn;
-    NBioAPI_DEVICE_ID nbioApiDeviceId;
-    
+    NBioAPI_DEVICE_ID nbioApiDeviceId = NBioAPI_DEVICE_ID_AUTO;
+
     options.add_options()
-        ("i,init", "Initialize SDK")
-        ("e,enroll", "Execute enroll function and return a hash")
-        ;
+        ("i,init", "Initialize SDK");
 
     auto result = options.parse(argc, argv);
 
     if (result.count("init")) {
         nbioApiReturn = NBioAPI_Init(&nbioApiHandle);
         if (nbioApiReturn != NBioAPIERROR_NONE) {
-            fmt::print("Failed to initialize SDK, Error code: {}", nbioApiReturn);
+            fmt::print("Failed to initialize SDK, Error code: {}\n", nbioApiReturn);
             return 1;
         }
-        fmt::print("Initialized handle\n");
-        return 0;
-    }
-
-    if (result.count("enroll")) {
-        nbioApiDeviceId = NBioAPI_DEVICE_ID_AUTO;
         nbioApiReturn = NBioAPI_OpenDevice(nbioApiHandle, nbioApiDeviceId);
         if (nbioApiReturn != NBioAPIERROR_NONE) {
-            fmt::print("Device failed to open\n");
+            fmt::print("Failed to open device, Error code: {}\n", nbioApiReturn);
             return 1;
         }
         fmt::print("Device opened\n");
-        return 0;
     }
 
     return 0;
